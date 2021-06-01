c3a07e0565936177a43aedcc2ccd36fd99fd4be4
aomi905 <hanna.s905@gmail.com>
2021-06-01 21:40:04 +0300

    Add entity classes

---
+++ src/main/java/bsu/frct/java/lab9/entity/Identifiable.java
@@ -0,0 +1,5 @@
+package bsu.frct.java.lab9.entity;
+
+public interface Identifiable {
+    int getId();
+}
---
+++ src/main/java/bsu/frct/java/lab9/entity/UserExistsException.java
@@ -0,0 +1,5 @@
+package bsu.frct.java.lab9.entity;
+
+public class UserExistsException extends Exception {
+    private static final long serialVersionUID = 584737643480913385L;
+}
